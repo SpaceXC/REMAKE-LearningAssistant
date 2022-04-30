4e06a662aab76386989b65b5c4ad5b68a59b5206
Liushilang <60663465+XC-Qan@users.noreply.github.com>
2022-04-30 22:49:55 +0800

    added ProfileFragment

--- app/src/main/java/com/bakamcu/remake/learningassistant/ProfileFragment.java
+++ app/src/main/java/com/bakamcu/remake/learningassistant/ProfileFragment.java
@@ -1,12 +1,20 @@
 package com.bakamcu.remake.learningassistant;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import cn.leancloud.LCUser;
 
 public class ProfileFragment extends Fragment {
 
@@ -26,5 +34,34 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView currentUser = requireView().findViewById(R.id.currentUser);
+        Button logout = requireView().findViewById(R.id.button);
+        currentUser.setText(LCUser.getCurrentUser().getUsername());
+        logout.setOnClickListener(view1 -> {
+            AlertDialog.Builder logoutDialogBuilder = new AlertDialog.Builder(requireActivity());
+            logoutDialogBuilder.setTitle("登出账号");
+            logoutDialogBuilder.setMessage("您确定要登出账号吗？");
+            logoutDialogBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(getContext(), LoginActivity.class));
+                    LCUser.logOut();
+                    requireActivity().finish();
+                }
+            });
+            logoutDialogBuilder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            logoutDialogBuilder.create();
+            logoutDialogBuilder.show();
+        });
     }
 }
