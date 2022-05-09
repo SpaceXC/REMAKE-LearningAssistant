c10a506fd716f4bbf50b550762215ec87e608af9
Liushilang <60663465+XC-Qan@users.noreply.github.com>
2022-05-09 20:40:48 +0800

    fixed some bugs of image displaying

--- app/src/main/java/com/bakamcu/remake/learningassistant/Leancloud.java
+++ app/src/main/java/com/bakamcu/remake/learningassistant/Leancloud.java
@@ -3,20 +3,21 @@ package com.bakamcu.remake.learningassistant;
 import static com.bakamcu.remake.learningassistant.AddProblem.TAG;
 
 import android.app.Application;
-import android.content.Intent;
 import android.util.Log;
 
 import cn.leancloud.LCLogger;
-import cn.leancloud.LCObject;
-import cn.leancloud.LCUser;
 import cn.leancloud.LeanCloud;
 
 public class Leancloud extends Application {
+    final static private String APP_ID = "1rG00zBIl97ITGRiXHTxpGa2-gzGzoHsz";
+    final static private String APP_KEY = "Fm77njR24SpW7RWbhJJvwvpG";
+    final static private String SERVER_URL = "https://xcapi.mengxiblog.top";
+
     @Override
     public void onCreate() {
         super.onCreate();
         LeanCloud.setLogLevel(LCLogger.Level.ALL);
-        LeanCloud.initialize(this, "1rG00zBIl97ITGRiXHTxpGa2-gzGzoHsz", "Fm77njR24SpW7RWbhJJvwvpG", "https://xcapi.mengxiblog.top");
+        LeanCloud.initialize(this, APP_ID, APP_KEY, SERVER_URL);
         Log.d(TAG, "onCreate: Instantiated Leancloud!");
     }
 }
